98e508813fc064fe386cc1c26c06ffc53bffe7a4
jahangiralamdiu <jahangiralamdiu@gmail.com>
2014-09-25 20:41:07 +0600

    use member variable autowire

--- src/com/pluralsight/service/CustomerServiceImpl.java
+++ src/com/pluralsight/service/CustomerServiceImpl.java
@@ -2,29 +2,20 @@ package com.pluralsight.service;
 
 import com.pluralsight.model.Customer;
 import com.pluralsight.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by lenovo on 25/09/2014.
  */
+
+@Service("customerService")
 public class CustomerServiceImpl implements CustomerService {
 
-    public CustomerServiceImpl() {
-    }
-
-    public CustomerServiceImpl(CustomerRepository customerRepository)
-    {
-
-        System.out.println("I am the result of constructor injection");
-        this.customerRepository = customerRepository;
-    }
-
-    public void setCustomerRepository(CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
-    CustomerRepository customerRepository;
+   @Autowired
+    private  CustomerRepository customerRepository;
 
     @Override
     public List<Customer> findAll()
